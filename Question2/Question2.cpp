61cee3f9b7e0e8ef418ad5455bdd5f5210c26326
Omkar Maurya <110991357+Omkar5001@users.noreply.github.com>
2023-12-26 14:27:06 +0530

    Add files via upload

---
+++ Question2/Question2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int N = 9;
+
+bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+    for (int i = 0; i < N; ++i) {
+        if (board[row][i] == num || board[i][col] == num || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(vector<vector<char>>& board) {
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < N; ++col) {
+            if (board[row][col] == '.') {
+                for (char num = '1'; num <= '9'; ++num) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[row][col] = '.'; 
+                    }
+                }
+                return false; 
+            }
+        }
+    }
+    return true; 
+    }
+
+void printSudoku(const vector<vector<char>>& board) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+
+    solveSudoku(board); 
+    printSudoku(board); //Solved Sudoku
+    return 0;
+}
